6ee0d96c5916f4afacc3878abed0f3abd3e41856
Martin Sørensen <superkai2@gmail.com>
2014-11-01 22:33:39 +0100

    Initial commit

---
+++ desktop/src/dk/gotler/ded/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dk.gotler.ded.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dk.gotler.ded.game.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
